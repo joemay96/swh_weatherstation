cb49a49ff98108982823eb167c082488608eb01d
Josef Mayer <little.joe96@gmx.de>
2023-02-15 11:59:15 +0100

    Temp sensor and libraries

--- Display.ino
+++ Display.ino
@@ -12,6 +12,8 @@ Grün [LEDA]8 -> 3V
 #include <Adafruit_GFX.h>    
 #include <Adafruit_ST7735.h> 
 #include <SPI.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 #define TFT_CS    D3//10
 #define TFT_RST   D4//8  
@@ -20,11 +22,19 @@ Grün [LEDA]8 -> 3V
 #define TFT_SCLK D5//13   
 #define TFT_MOSI D7//11   
 
+//TEMP
+#define T_H_PIN D2
+#define DHT_TYPE DHT22
+DHT_Unified dht(T_H_PIN, DHT_TYPE);
+
+//Pressure Sensor
+
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 void setup(void) {
   Serial.begin(9600);
 
+  /* Display */
   // tft.setRotation(tft.getRotation()+1);
   tft.initR(INITR_BLACKTAB);
   tft.fillScreen(ST7735_BLACK);
@@ -37,17 +47,47 @@ void setup(void) {
   tft.setCursor(30,55);
   tft.println("Hello World!");  
   delay(1000);
+
+  // Temp und Humidiy
+  dht.begin();
   
 }
 
 void loop() {
+  // Display
+  printToDisplay();
+  // Temp and Humidity
+  getHumidityAndTemp();
+}
 
+void printToDisplay() {
   tft.fillScreen(ST7735_WHITE); 
   delay(1000);
   tft.setTextColor(ST7735_BLACK);
   tft.setTextSize(0);
   tft.setCursor(30,80);
   tft.println("Hey you! You got it!");  
-
   delay(500);
+}
+
+void getHumidityAndTemp() {
+  // float h = dht.Humidity();
+  // float t = dht.Temperature();
+  sensors_event_t event;
+  dht.humidity().getEvent(&event);
+  float h = event.relative_humidity;
+  dht.temperature().getEvent(&event);
+  float t = event.temperature;
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Serial.print("Humidity: "); 
+  Serial.print(String(h));
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.println(" *C ");
+  delay(2000);
 }
